42229811b2574dffd0019330bb283b5971fbee8b
Matthew Parkes <mp4@ad.datcon.co.uk>
2022-12-05 18:05:08 +0000

    day 5

---
+++ day5a.cpp
@@ -0,0 +1,37 @@
+#include "utils.hpp"
+
+
+int main(int argc, char *argv[])
+{
+    // Cheating because lazy
+    std::vector<std::string> crates = {
+        "",
+"WRF",
+"THMCDVWP",
+"PMZNL",
+"JCHR",
+"CPGHQTB",
+"GCWLFZ",
+"WVLQZJGC",
+"PNRFWTVC",
+"JWHGRSV",
+    };
+    auto input = read_input("day5_input");
+    std::regex reg_exp("([0-9]+) [a-z]+ ([0-9]+) [a-z]+ ([0-9]+)");
+    std::smatch reg_out;
+    for (auto &&x : input)
+    {
+        if (std::regex_search(x, reg_out, reg_exp))
+        {
+            for (int y = 0; y < std::stoi(reg_out[1]); y++)
+            {
+                crates.at(std::stoi(reg_out[3])).push_back(crates.at(std::stoi(reg_out[2])).back());
+                crates.at(std::stoi(reg_out[2])).pop_back();
+            }
+        }
+    }
+    for (auto && y: crates)
+    {
+        std::cout << y << "\n";
+    }
+}
